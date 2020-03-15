6d0f10f8322b72d7dd250c1a4e532cc0416bc1ee
Rohit Saxena <rohitmsaxena@gmail.com>
2020-03-14 20:17:54 -0500

    Added sample component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {ApplicationRef, DoBootstrap, Injector, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {SampleModule} from './sample/sample.module';
+import {createCustomElement} from '@angular/elements';
+import {SampleComponent} from './sample/sample.component';
 
+const local = false;
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SampleModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [SampleComponent],
+  bootstrap: [local ? AppComponent : []]
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+  constructor(private injector: Injector) {
+    const micro = createCustomElement(SampleComponent, {injector: this.injector});
+    customElements.define('micro-app', micro);
+  }
+  ngDoBootstrap(appRef: ApplicationRef): void {
+  }
+}
